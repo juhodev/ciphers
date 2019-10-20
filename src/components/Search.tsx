dcc900ef396863f8594bbcc6bee8cb6f6f45b9e0
juhodev <jooo.dev@gmail.com>
2019-10-20 15:42:47 +0300

    Refactor search box
    Now the search box lists the suggestions under it and gives some basic info about the machine

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,32 +1,74 @@
 import * as React from 'react';
 import './Search.scss';
-import { Sigaba } from '../machines/sigaba/Sigaba';
+import { MACHINE_BASIC_INFO, BasicInfo } from './Types';
 
 interface SearchProps {
 	history: any;
 }
 
-export default class Search extends React.Component<SearchProps> {
+interface SearchState {
+	input: string;
+}
+
+export default class Search extends React.Component<SearchProps, SearchState> {
 	constructor(props: any) {
 		super(props);
-	}
 
-	componentDidMount() {
-		const sigaba: Sigaba = new Sigaba();
-		sigaba.encryptString();
+		this.state = { input: '' };
 	}
 
 	render() {
+		const { input } = this.state;
+
 		return (
 			<div className="search">
 				<input
 					placeholder="Search"
 					onChange={e => {
-						this.find(e.target.value);
+						this.setState({ input: e.target.value });
 					}}
 				/>
+				{this.renderSuggestions(this.findSuggestions(input))}
 			</div>
 		);
+	}
+
+	renderSuggestions(suggestions: BasicInfo[]): JSX.Element[] {
+		return suggestions.map(suggestion => {
+			return (
+				<div className="suggestion">
+					<span
+						className="machine"
+						onClick={() => this.props.history.push(suggestion.link)}
+					>
+						{suggestion.name}
+					</span>
+					<div className="suggestion-info">
+						<span className="date">Date: {suggestion.date}</span>
+						<span className="country">
+							Country: {suggestion.country}
+						</span>
+					</div>
+				</div>
+			);
+		});
+	}
+
+	findSuggestions(input: string): BasicInfo[] {
+		const suggestions: BasicInfo[] = MACHINE_BASIC_INFO;
+
+		if (input.length === 0) {
+			return suggestions;
+		}
+
+		const lowerCaseInput: string = input.toLowerCase();
+
+		// This might be the worst way to do this but don't have time to use something like this https://en.wikipedia.org/wiki/Levenshtein_distance
+		const validSuggestions: BasicInfo[] = suggestions.filter(s =>
+			s.name.toLowerCase().startsWith(lowerCaseInput),
+		);
+
+		return validSuggestions;
 	}
 
 	find(value: string) {
--- src/components/Types.ts
+++ src/components/Types.ts
@@ -8,3 +8,25 @@ export interface Cam {
 	y: number;
 	position: number;
 }
+
+export interface BasicInfo {
+	name: string;
+	date: string;
+	country: string;
+	link: string;
+}
+
+export const MACHINE_BASIC_INFO: BasicInfo[] = [
+	{
+		name: 'Enigma',
+		date: '1923',
+		country: 'Germany',
+		link: '/enigma',
+	},
+	{
+		name: 'Lorenz cipher (Lorenz SZ40)',
+		date: '1940 or something',
+		country: 'Germany',
+		link: '/lorenz',
+	},
+];
